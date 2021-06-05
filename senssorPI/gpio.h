fb119e9b13fcc9da3f1a1983470fa7943dd928d9
Jihun Hwang <goharrm@ajou.ac.kr>
2021-06-05 21:26:05 +0900

    gpio 라이브러리 수정

--- senssorPI/gpio.h
+++ senssorPI/gpio.h
@@ -15,13 +15,13 @@
 #define LOW 0
 #define HIGH 1
 
-static int GPIOExport(int pin){
+static int GPIOExport(int pin) {
     char buffer[BUFFER_MAX];
     ssize_t bytes_written;
     int fd;
 
     fd = open("/sys/class/gpio/export", O_WRONLY);
-    if(fd == -1){
+    if (fd == -1) {
         fprintf(stderr, "Failed to open export for writing!\n");
         return(-1);
     }
@@ -32,13 +32,13 @@ static int GPIOExport(int pin){
     return(0);
 }
 
-static int GPIOUnexport(int pin){
+static int GPIOUnexport(int pin) {
     char buffer[BUFFER_MAX];
     ssize_t bytes_written;
     int fd;
 
     fd = open("/sys/class/gpio/unexport", O_WRONLY);
-    if(fd == -1){
+    if (fd == -1) {
         fprintf(stderr, "Failed to open unexport for writing!\n");
         return(-1);
     }
@@ -49,7 +49,7 @@ static int GPIOUnexport(int pin){
     return(0);
 }
 
-static int GPIODirection(int pin, int dir){
+static int GPIODirection(int pin, int dir) {
     static const char s_direcctions_str[] = "in\0out";
 
     char path[DIRECTION_MAX] = "/sys/class/gpio/gpio%d/direction";
@@ -58,12 +58,12 @@ static int GPIODirection(int pin, int dir){
     snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
 
     fd = open(path, O_WRONLY);
-    if(fd == -1){
+    if (fd == -1) {
         fprintf(stderr, "Failed to open gpio direction for writing!\n");
         return(-1);
     }
 
-    if(-1 == write(fd, &s_direcctions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)){
+    if (-1 == write(fd, &s_direcctions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)) {
         fprintf(stderr, "Failed to set direction!\n");
         return(-1);
     }
@@ -72,19 +72,19 @@ static int GPIODirection(int pin, int dir){
     return(0);
 }
 
-static int GPIORead(int pin){
+static int GPIORead(int pin) {
     char path[VALUE_MAX];
     char value_str[3];
     int fd;
 
     snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
     fd = open(path, O_RDONLY);
-    if(fd == -1){
+    if (fd == -1) {
         fprintf(stderr, "Failed to open gpio value for reading!\n");
         return(-1);
     }
 
-    if(-1 == read(fd, value_str, 3)){
+    if (-1 == read(fd, value_str, 3)) {
         fprintf(stderr, "Failed to read value!\n");
         return(-1);
     }
@@ -94,7 +94,7 @@ static int GPIORead(int pin){
     return(atoi(value_str));
 }
 
-static int GPIOWrite(int pin, int value){
+static int GPIOWrite(int pin, int value) {
     static const char s_values_str[] = "01";
 
     char path[VALUE_MAX];
@@ -102,12 +102,12 @@ static int GPIOWrite(int pin, int value){
 
     snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
     fd = open(path, O_WRONLY);
-    if(fd == -1){
+    if (fd == -1) {
         fprintf(stderr, "Failed to open gpio value for writing!\n");
         return(-1);
     }
 
-    if(1 != write(fd, &s_values_str[LOW == value ? 0 : 1], 1)){
+    if (1 != write(fd, &s_values_str[LOW == value ? 0 : 1], 1)) {
         fprintf(stderr, "Failed to set direction!\n");
         return(-1);
     }
